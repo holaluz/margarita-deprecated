b76a2c7630e5499dbea29eb88db8d5ac4a671a80
Adrià Fontcuberta <afontcu@gmail.com>
2020-12-24 09:08:36 +0100

    Remove unused CSS

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import './scss/_main.scss'
-
 import MaGridColumn from './components/MaGridColumn'
 import MaGridContainer from './components/MaGridContainer'
 import MaGridRow from './components/MaGridRow'
